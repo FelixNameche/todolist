fe5e143fa983f56450c00b575d2b0ab88afb764d
KevinDubreucq <kevin.dubreucq@gmail.com>
2018-03-01 16:33:43 +0100

    Fin de journée

--- contenu.php
+++ contenu.php
@@ -1,33 +1,51 @@
 <?php
-    ini_set("dispay_errors",0);error_reporting(0);
-    $filename="todo.json";
-    $contenu_json = file_get_contents($filename);
-    $receipt = json_decode($contenu_json, true);
+
+try
+{
+	// On se connecte à MySQL
+	$db = new PDO('mysql:host=localhost;dbname=todolist;charset=utf8', 'root', 'user');
+}
+catch(Exception $e)
+{
+	// En cas d'erreur, on affiche un message et on arrête tout
+    die('Erreur : '.$e->getMessage());
+}
+
+if ($conn->connect_error){
+    die("connection failed: " . $conn->connect_error);
+
+}
+echo "connected succefully";
+    // ini_set("dispay_errors",0);error_reporting(0);
+    // $filename="todo.json";
+    // $contenu_json = file_get_contents($filename);
+    // $receipt = json_decode($contenu_json, true);
     
     // bouton ajouter
     if (isset($_POST['submit']) AND end($receipt)['Nom'] != $_POST['addtask']){
         $add_tache = $_POST['addtask'];
         $array_tache = array("Nom" => $add_tache,
-                             "Terminer" => false);
+                            "Terminer" => false);
         $receipt[] = $array_tache;
-        $json= json_encode($receipt, JSON_PRETTY_PRINT);
-        file_put_contents($filename, $json);
-        $receipt = json_decode($json, true);
+
+        $dbadd = "INSERT INTO dbtask (task, stat) 
+        VALUES ('".$addtask."', 'False')";
+        // $json= json_encode($receipt, JSON_PRETTY_PRINT);
+        // file_put_contents($filename, $json);
+        // $receipt = json_decode($json, true);
     }
     
     // bouton enregistrer
     if (isset($_POST['save'])){
         $choix=$_POST['addtask'];
-
         for ($init = 0; $init < count($receipt); $init ++){
             if (in_array($receipt[$init]['Nom'], $choix)){
-              $receipt[$init]['Terminer'] = true;
+            $receipt[$init]['Terminer'] = true;
             }
         }
-
-        $json= json_encode($receipt, JSON_PRETTY_PRINT);
-        file_put_contents($filename, $json);
-        $receipt = json_decode($json, true);
+        // $json= json_encode($receipt, JSON_PRETTY_PRINT);
+        // file_put_contents($filename, $json);
+        // $receipt = json_decode($json, true);
     }
 
     // bouton retirer
@@ -36,14 +54,70 @@
         // var_dump($choix);
         for ($init = 0; $init < count($receipt); $init ++){
             if (!in_array($receipt[$init]['Nom'], $choix)){
-              $receipt[$init]['Terminer'] = false;
+            $receipt[$init]['Terminer'] = false;
             }
         }
-
-        $json= json_encode($receipt, JSON_PRETTY_PRINT);
-        file_put_contents($filename, $json);
-        $receipt = json_decode($json, true);
+        // $json= json_encode($receipt, JSON_PRETTY_PRINT);
+        // file_put_contents($filename, $json);
+        // $receipt = json_decode($json, true);
     }
+
+    // if(isset($_POST['submit'])){
+
+    //     $options = array(
+    //     'addtask' => FILTER_SANITIZE_STRING);
+    
+    //     $result = filter_input_array(INPUT_POST, $options);
+    //     $checkResult =[];
+    
+    //     $addtask = trim($result['addtask']);
+    
+    //     if(isset($addtask) AND !empty($addtask) ){
+    //     $verif_addtask = "ok";
+    //     }
+    //     else {
+    //     $verif_addtask = "pok";
+    //     }
+    // }
+
+    // if(isset($_POST['save'])){
+
+    //     $options = array(
+    //     'addtask[]' => FILTER_SANITIZE_STRING);
+        
+    //     $result = filter_input_array(INPUT_POST, $options);
+    //     $checkResult =[];
+        
+    //     $addtaskarray = trim($result['addtask[]']);
+        
+    //     if(isset($addtaskarray) AND !empty($addtaskarray) ){
+    //     $verif_addtaskarray = "ok";
+    //     }
+    //     else {
+    //     $verif_addtaskarray = "pok";
+    //     }
+    // }
+
+    // if(isset($_POST['unsave'])){
+
+    //     $options = array(
+    //     'removetask[]' => FILTER_SANITIZE_STRING);
+    
+    //     $result = filter_input_array(INPUT_POST, $options);
+    //     $checkResult =[];
+    
+    //     $removetask = trim($result['removetask[]']);
+    
+    //     if(isset($removetask) AND !empty($removetask) ){
+    //     $verif_removetask = "ok";
+    //     }
+    //     else {
+    //     $verif_removetask = "pok";
+    //     }
+    // }
+    
+    var_dump($addtask);
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -64,16 +138,19 @@
                     <form action="contenu.php" method="post" name="formafaire">
                         <div class="todotask">
                             <?php
-                                foreach ($receipt as $key => $value){
-                                    if ($value["Terminer"] == false){
-                                        echo "<input type='checkbox' name='addtask[]' value='".$value["Nom"]."'/>
-                                            <label for='choix'>".$value["Nom"]."</label><br />";
-                                    }
-                                }
+                                // foreach ($receipt as $key => $value){
+                                //     if ($value["Terminer"] == false){
+                                         echo "<input type='checkbox' name='addtask[]' value='".($value["Nom"])."'/>
+                                            <label for='choix'>".($value["Nom"])."</label><br />";
+                                        $result = $db->query('SELECT * FROM dbtask WHERE stat="0"');
+                                        $resultArr = $result->fetchAll(PDO::FETCH_ASSOC);
+                                        print_R($resultArr);
+                                //     }
+                                // }
                             ?>
                         </div>
                         <div class="buttonretrait">
-                            <input type="submit" name="save" value="Enregistrer">
+                            <input id="buttonId" type="submit" name="save" value="Enregistrer">
                         </div>
                     </form>
                 </section>
@@ -84,17 +161,26 @@
                     <form action="contenu.php" method="post" name="formchecked">
                         <div class="listarchives">
                             <?php
-                                foreach ($receipt as $key => $value){
-                                    if ($value["Terminer"] == true){
+                                // foreach ($receipt as $key => $value){
+                                //     if ($value["Terminer"] == true){
+                                    
+                                    echo "<input type='checkbox' name='removetask[]' value='".$value["Nom"]."'checked/>
+                                    <label for='choix'>".$value["Nom"]."</label><br />";
+                                        $result = $db->query('SELECT * FROM dbtask WHERE stat="1"');
+                                        $resultArr = $result->fetchAll(PDO::FETCH_ASSOC);
+                                    print_R($resultArr);
 
-                                        echo "<input type='checkbox' name='removetask[]' value='".$value["Nom"]."'checked/>
-                                            <label for='choix'>".$value["Nom"]."</label><br />";
-                                    }
-                                }
+                                    $dbup = "UPDATE dbtask SET stat = 'True' WHERE task='".$key."'";
+                                    //Si nomtache est égale à la valeur checkée, remplacement de 'False' par 'True'
+    
+                                    $resultat = $db->exec($dbup); // Exécution... ( query )
+    
+                                //     }
+                                // }
                             ?>
                         </div>
                         <div class="buttonretrait">
-                            <input type="submit" name="unsave" value="Retirer">
+                            <input id="buttonId2" type="submit" name="unsave" value="Retirer">
                         </div>
                     </form>
                 </section>
@@ -108,6 +194,12 @@
                     </div>
                     <div class="add">
                         <form class="" action="contenu.php" method="post">
+                            <?php
+                                $dbadd = "INSERT INTO dbtask (task, stat) VALUES ('".$submit."', 'False')";
+                        //         //Ajout de la tache dans la database avec la valeur 'False'
+                                $resultat = $db->exec($dbadd);
+                        //         //execution de la requête sur la base de donnée
+                            ?>
                             <input type="text" name="addtask" value="">
                             <input type="submit" name="submit" value="Ajouter">
                         </form>
@@ -118,6 +210,8 @@
         </section>
     </body>
     <footer>
+        <h2>Created by Félix Namêche and Kevin Dubreucq</h2>
+        <h3>In Becode Formation 2018 Promo Charleroi<h3>
         <audio autoplay loop="">
             <source src="https://archive.org/download/nyannyannyan/NyanCatoriginal.ogg" type="audio/ogg">
         </audio>
